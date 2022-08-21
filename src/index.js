00780813090739a7559d73ae06517c552b3d2039
ul1ra <benpetercunningham@gmail.com>
2022-08-21 16:52:06 +0200

    uncommented stupid mistake

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from '../src/main-component/App/App';
 import reportWebVitals from './reportWebVitals';
 import './css/font-awesome.min.css';
 import './css/themify-icons.css';
-// import './css/flaticon.css';
+import './css/flaticon.css';
 import './css/animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/style.scss';
